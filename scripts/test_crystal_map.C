20458e695692ee430c32df5cc4c344900949c7d5
murat <murat@fnal.gov>
2016-01-19 11:56:57 -0600

    - update

--- scripts/test_crystal_map.C
+++ scripts/test_crystal_map.C
@@ -4,19 +4,41 @@ to run:
 -------
 root.exe
 .L lib/libmu2e_CalorimeterGeom.so
-.L test_crystal_map.cc+
-test_crystal_map()
+.L test_crystal_map.C++
+test1(5)
+test2()
 
 ///////////////////////////////////////////////////////////////////////////// */
 
 #include "CalorimeterGeom/inc/HexMapper.hh"
 #include "CalorimeterGeom/inc/SquareMapper.hh"
 
-int test_crystal_map() {
+
+//-----------------------------------------------------------------------------
+// print out the crystal numerology
+//-----------------------------------------------------------------------------
+int test1(int N) {
+
+  mu2e::SquareMapper map;
+  
+  for (int i=0; i<N; i++) {
+     mu2e::SquLK x = map.lk(i);
+
+    printf("i, x._i, x._y = %5i %5i %5i\n",i, x._l, x._k);
+  }
+  
+  return 0;
+}
+
+
+
+//-----------------------------------------------------------------------------
+// compare hex and square map definitions of nRings
+//-----------------------------------------------------------------------------
+int test2() {
 
   mu2e::HexMapper    hm;
   mu2e::SquareMapper sm;
-
 
   printf(" hm.nCrystalMax(1) = %5i sm.nCrystalMax(1) = %5i\n",
 	 hm.nCrystalMax(1), sm.nCrystalMax(1));

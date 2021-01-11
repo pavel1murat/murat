68c9f06e0d6616635c537fcca9ea156257f2b0f4
Pavel Murat <murat@fnal.gov>
2021-01-11 10:30:15 -0600

    use MVAs training from murat/data

--- ana/mva_data.cc
+++ ana/mva_data.cc
@@ -29,63 +29,63 @@ mva_data::data_t  trq_mva_andy_0100 = {
 mva_data::data_t  trq_mva_fele2s51b1_0070 = {
   "fele2s51b1_0070",                                                         // name
   "nactive:nafract:log10(fcons):momerr:t0err:nda_o_na:nza_o_na:nma_o_nm",    // assume all floats
-  "su2020/data/trq_mva/MLP_weights_0070.xml",                                // location of XML weights
+  "murat/data/trq_mva/MLP_weights_0070.xml",                                // location of XML weights
   0.40                                                                       // default cut value
 };
 
 mva_data::data_t  trq_mva_fele2s51b1_0060 = {
   "fele2s51b1_060",                                                          // name
   "nactive:nafract:log10(fcons):momerr:t0err:nda_o_na:nza_o_na:nma_o_nm",    // assume all floats
-  "su2020/data/trq_mva/MLP_weights_0060.xml",                                // location of XML weights
+  "murat/data/trq_mva/MLP_weights_0060.xml",                                // location of XML weights
   0.80                                                                       // default cut value
 };
 
 mva_data::data_t  trq_mva_fele2s51b1_1055 = {                                    // 
   "fele2s51b1_1055",                                                         // name
   "nactive:nafract:log10(fcons):momerr:t0err:nda_o_na:nza_o_na:nma_o_nm",    // assume all floats
-  "su2020/data/trq_mva/MLP_weights_1055.xml",                                // location of XML weights
+  "murat/data/trq_mva/MLP_weights_1055.xml",                                // location of XML weights
   0.20                                                                       // default cut value
 };
 
 mva_data::data_t  trq_mva_fele2s51b1_1060 = {                                    // 
   "fele2s51b1_1060",                                                         // name
   "nactive:nafract:log10(fcons):momerr:t0err:nda_o_na:nza_o_na:nma_o_nm",    // assume all floats
-  "su2020/data/trq_mva/MLP_weights_1060.xml",                                // location of XML weights
+  "murat/data/trq_mva/MLP_weights_1060.xml",                                // location of XML weights
   0.20                                                                       // default cut value
 };
 
 mva_data::data_t  trq_mva_fele2s51b1_1070 = {
   "fele2s51b1_1070",                                                         // name
   "nactive:nafract:log10(fcons):momerr:t0err:nda_o_na:nza_o_na:nma_o_nm",    // assume all floats
-  "su2020/data/trq_mva/MLP_weights_1070.xml",                                // location of XML weights
-  0.20                                                                       // default default cut value
-};
-
-mva_data::data_t  trq_mva_fpos2s51b1_0150 = {
-  "fpos2s51b1_0150",                                                         // positrons, PAR dpF>0.5 MeV/c
-  "nactive:nafract:log10(fcons):momerr:t0err:nda_o_na:nza_o_na:nma_o_nm",    // assume all floats
-  "su2020/data/trq_mva/MLP_weights_0150.xml",                                // location of XML weights
+  "murat/data/trq_mva/MLP_weights_1070.xml",                                // location of XML weights
   0.20                                                                       // default default cut value
 };
 
 mva_data::data_t  trq_mva_fpos2s51b1_0170 = {
-  "fpos2s51b1_0170",                                                         // positrons, PAR dpF>0.7 MeV/c
+  "fpos2s51b1_0170",                                                         // positrons, DAR dpF>0.7 MeV/c
   "nactive:nafract:log10(fcons):momerr:t0err:nda_o_na:nza_o_na:nma_o_nm",    // assume all floats
-  "su2020/data/trq_mva/MLP_weights_0170.xml",                                // location of XML weights
+  "murat/data/trq_mva/MLP_weights_0170.xml",                                // location of XML weights
   0.20                                                                       // default default cut value
 };
 
 mva_data::data_t  trq_mva_fpos2s51b1_1150 = {
   "fpos2s51b1_1150",                                                         // positrons, DAR dpF>0.5 MeV/c
   "nactive:nafract:log10(fcons):momerr:t0err:nda_o_na:nza_o_na:nma_o_nm",    // assume all floats
-  "su2020/data/trq_mva/MLP_weights_1150.xml",                                // location of XML weights
+  "murat/data/trq_mva/MLP_weights_1150.xml",                                // location of XML weights
+  0.20                                                                       // default default cut value
+};
+
+mva_data::data_t  trq_mva_fpos2s51b1_1152 = {
+  "fpos2s51b1_1152",                                                         // positrons, DAR dpF>0.5 MeV/c  exp
+  "nactive:nafract:log10(fcons):momerr:t0err:nda_o_na:nza_o_na:nma_o_nm",    // assume all floats
+  "murat/data/trq_mva/MLP_weights_1152.xml",                                // location of XML weights
   0.20                                                                       // default default cut value
 };
 
 mva_data::data_t  trq_mva_fpos2s51b1_1170 = {
   "fpos2s51b1_1170",                                                         // positrons, DAR dpF>0.7 MeV/c
   "nactive:nafract:log10(fcons):momerr:t0err:nda_o_na:nza_o_na:nma_o_nm",    // assume all floats
-  "su2020/data/trq_mva/MLP_weights_1170.xml",                                // location of XML weights
+  "murat/data/trq_mva/MLP_weights_1170.xml",                                // location of XML weights
   0.20                                                                       // default default cut value
 };
 
@@ -95,7 +95,7 @@ mva_data::data_t  trq_mva_fpos2s51b1_1170 = {
 mva_data::data_t  pid_mva_ele00s61b0_1000 = {
   "pid_ele00s61b0_1000",                                                     // name
   "ecl/p:ncr:seedfr:ele_dt:ele_dz:ele_dr:ele_path",                          // assume all floats
-  "su2020/data/pid_mva/pid_MLP_weights_1000.xml",                            // location of XML weights
+  "murat/data/pid_mva/pid_MLP_weights_1000.xml",                            // location of XML weights
   0.50                                                                       // default cut value
 };
 
@@ -127,9 +127,9 @@ mva_data::mva_data(const char* Dataset, int TrainingCode) {
     else                           { error = 1; }
   }
   if      (ds == "FPOS2S51B1") {
-    if      (TrainingCode ==  150) fData = trq_mva_fpos2s51b1_0150;  // PAR, logfcons, uniform weight, dPf > 0.5
-    else if (TrainingCode ==  170) fData = trq_mva_fpos2s51b1_0170;  // PAR, logfcons, uniform weight, dPf > 0.7
+    if      (TrainingCode ==  170) fData = trq_mva_fpos2s51b1_0170;  // PAR, logfcons, uniform weight, dPf > 0.7
     else if (TrainingCode == 1150) fData = trq_mva_fpos2s51b1_1150;  // DAR, logfcons, uniform weight, dPf > 0.5
+    else if (TrainingCode == 1152) fData = trq_mva_fpos2s51b1_1152;  // DAR, logfcons, exp     weight, dPf > 0.5
     else if (TrainingCode == 1170) fData = trq_mva_fpos2s51b1_1170;  // DAR, logfcons, uniform weight, dPf > 0.7
     else                           { error = 1; }
   }
